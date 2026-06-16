4fc4c3f3f41964532b322398aad09460e16a2c71
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-06-16 12:16:38 +0700

    solving the challenge 1 of section 8

---
+++ program/section_8/challenge_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<Stdlib.h>
+
+// PRINTING ASTERISK RECTANGLE
+
+int main(){
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    return 0;
+}
