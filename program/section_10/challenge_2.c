d698d89484733c421583418dd5190808fcdb3b2e
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-06-21 11:10:15 +0700

    solving the the challenge 2 in the section 10

--- program/section_10/challenge_2.c
+++ program/section_10/challenge_2.c
@@ -3,9 +3,21 @@
 #include<math.h>
 #include<string.h>
 
-// 
+// A PROGRAM TO CALCULATE THE SUM OF THE SEQUENCE
 
 int main(void){
-    
+    int a1,an,n,sn;
+    printf("==== SUM OF THE SEQUENCE PROGRAM ====\n");
+    printf("Please input the first term of the sequence (a1): \n> ");
+    scanf("%d",&a1);
+    printf("Please input the nth term of the sequence (an): \n> ");
+    scanf("%d",&an);
+    printf("Please input the position of the sequence (n): \n> ");
+    scanf("%d",&n);
+    sn =  (a1 + an) * n/2;
+    printf("The sum of the sequence (sn) is %d.\n", sn);
+
+
+
     return 0;
 }
