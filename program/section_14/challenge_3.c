ee15a6d3c922b82646c1dfc6411c336522d314b5
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-07-14 22:24:55 +0700

    complete challenge_3

--- program/section_14/challenge_3.c
+++ program/section_14/challenge_3.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
+#include<io.h>
+#include<math.h>
 // PROGRAM THAT RECEIVES AN INTEGER VALUE AND PRINT ITS ABSOLUTE VALUE
 // EXAMPLE: -10 --> |10| = 10
 
 int main(void){
     int negativeNum;
-    printf("Enter a negative number: \n> ");
+    int result;
+    printf("Enter a negative integer number: \n> ");
+    scanf("%d", &negativeNum);
+    if (negativeNum >= 0){
+        result = negativeNum;
+    }
+    else{
+        result = negativeNum * -1;
+    }
+    printf("The result of  |%d| is %d", negativeNum, result);
     return 0;
 }
