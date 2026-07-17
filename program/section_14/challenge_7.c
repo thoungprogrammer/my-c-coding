5558617c7273f6c198feebc0363cd5606e5e88c9
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-07-17 11:31:37 +0700

    just solving challenge_7 in section 14 but not completed

---
+++ program/section_14/challenge_7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+// PROGRAM THAT RECEIVES 3 INTEGERS IF THAT THREE INTEGER DIVIDE PRINT DIVISIBLE
+
+int main(void){
+    int firstNum;int secNum;int thirdNum;
+    printf("Enter three number \n>")
+    return 0;
+}
