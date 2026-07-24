0a89c28b16ce874fbb6b281d9eed915b8d209520
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-07-24 15:33:26 +0700

    solve challenge 7

--- program/section_14/challenge_7.c
+++ program/section_14/challenge_7.c
@@ -3,7 +3,18 @@
 // PROGRAM THAT RECEIVES 3 INTEGERS IF THAT THREE INTEGER DIVIDE PRINT DIVISIBLE
 
 int main(void){
+    int threeNum;
     int firstNum;int secNum;int thirdNum;
-    printf("Enter three number \n>")
+    printf("Enter three integer number \n>");
+    scanf("%d", &threeNum);
+    firstNum = (threeNum % 10) % 10;
+    thirdNum = threeNum / 100;
+    secNum = (threeNum - thirdNum * 100) / 10;
+    if (firstNum % secNum == 0 || secNum % thirdNum == 0 || firstNum % thirdNum == 0){
+        printf("DIVIDER");
+    }
+    else {
+        printf("CANNOT DIVIDE");
+    }    
     return 0;
 }
